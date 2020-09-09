fa3cfa445c01d6e184a731b69bcfede9d80ee398
certafonso <44088727+certafonso@users.noreply.github.com>
2020-09-09 23:14:23 +0100

    Added LEDs and bug fixes

--- Project_Morse.ino
+++ Project_Morse.ino
@@ -1,5 +1,6 @@
 const int Button_pin = 2;
-const int LED_pin = 13;
+const int green_pin = 3;
+const int red_pin = 4;
 const int dt = 1000;
 int time_on = 0;
 int time_off = 0;
@@ -23,7 +24,8 @@ void setup()
 
   // configure pins
   pinMode(Button_pin, INPUT_PULLUP);
-  pinMode(LED_pin, OUTPUT);
+  pinMode(green_pin, OUTPUT);
+  pinMode(red_pin, OUTPUT);
 }
 
 void loop() 
@@ -31,30 +33,27 @@ void loop()
   int button = !digitalRead(Button_pin);
 
   if (button) {   // button is active
-    digitalWrite(LED_pin, HIGH);
+    digitalWrite(green_pin, HIGH);
+    digitalWrite(red_pin, HIGH);
     time_on++;
     time_off = 0;
     
   } else {       // button is inactive
-    digitalWrite(LED_pin, LOW);
+    digitalWrite(green_pin, LOW);
+    digitalWrite(red_pin, LOW);
 
     if (time_on){   // button just became inactive
     letter = letter << 1;  // makes space for new bit
     len_letter ++;
 
-      if (time_on > 25){  // dash (1)
-        Serial.println("dash");
+      if (time_on > 25){  // dash (1) dot is 0, nothing is needed
         letter++;
       }
-      else{
-        Serial.println("dot");
-      }
-      Serial.println(letter);
     }
     else{
       if (time_off == 75){  // pause means the letter ended
-        Serial.println("letter");
-        Serial.println((int) letter);
+        dodecode();
+        len_letter = 0;
       }
     }
 
@@ -65,15 +64,47 @@ void loop()
   delay(10);
 }
 
+void dodecode()
+{   // decodes the letter, prints the result
+    char decoded;
+
+    if(len_letter <= 6){
+      decoded = decode();
+      
+      if(decoded){
+        if(decoded == 1){
+          Serial.println("Caracter not supported");
+        }
+        else{
+          Serial.println(decoded);  
+        }
+        digitalWrite(green_pin, HIGH);
+        delay(100);
+        digitalWrite(green_pin, LOW);
+        return;
+      }
+      else{
+        Serial.println("Invalid caracter");
+      }
+    }
+    else{
+      Serial.println("Error");
+    }
+    digitalWrite(red_pin, HIGH);
+    delay(100);
+    digitalWrite(red_pin, LOW);
+}
+
 char decode()
 {   // decodes the morse word to char
   char mask = 0;
-  
-  for(int i=0; i++; i<len_letter){  // creates the mask to filter bits that aren't needed
-    mask += (char) pow(2, i);
+  char i=0;
+
+  for(i; i<len_letter; i++){  // creates the mask to filter bits that aren't needed
+    mask += (char) round(pow(2, i));
   }
 
   letter = letter & mask; // apply mask
 
-  return dict[len_letter][letter];
+  return dict[len_letter-1][letter];
 }
