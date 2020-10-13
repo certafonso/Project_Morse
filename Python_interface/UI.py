e93a17e532c3e65926d38cb2ff4119a9c7b27d93
certafonso <44088727+certafonso@users.noreply.github.com>
2020-10-13 20:21:44 +0100

    UI now also shows the word in morse

--- Python_interface/UI.py
+++ Python_interface/UI.py
@@ -1,5 +1,13 @@
+"""
+A simple UI that just shows the letter inputed by user on the screen
+"""
+
 import serial
 import pygame
+import json
+
+with open("./Python_interface/Morse.json") as f:
+	morse_dict = json.load(f)
 
 ser = serial.Serial('COM5', 9600)
 
@@ -17,6 +25,7 @@ display_surface = pygame.display.set_mode((X, Y))
 pygame.display.set_caption("Project Morse") 
 
 word = ""
+word2 = ""
 
 # create a font object
 font = pygame.font.Font('freesansbold.ttf', X // 2) 
@@ -25,18 +34,23 @@ while True :
 	# get serial
 	if ser.inWaiting():
 		word = ser.read(1)
-		type(word)
-		print(word)
+
+		try:
+			word2 = morse_dict[word.decode()]
+		except: pass
+
+	display_surface.fill(white)
 
 	# update text
 	text = font.render(word, True, black) 
-	textRect = text.get_rect()
-	textRect.center = (X // 2, Y // 2) 
-  
-	# show stuff
-	display_surface.fill(white)
+	textRect = text.get_rect(center=(X // 2, Y // 3))
 	display_surface.blit(text, textRect) 
 
+	# update text
+	text2 = font.render(word2, True, black)
+	textRect2 = text2.get_rect(center=(X // 2, 2 * Y // 3))
+	display_surface.blit(text2, textRect2) 
+  
 	for event in pygame.event.get() : 
 		if event.type == pygame.QUIT : 
 
