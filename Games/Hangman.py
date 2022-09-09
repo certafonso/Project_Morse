0878588bdea0deb2a7ca4d18645dbc23f08c6b10
certafonso <certafonso@gmail.com>
2022-09-09 21:01:09 +0100

    Hangman.py now receives COM port and word via argv Added better documentation do Hangman.py

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -1,4 +1,9 @@
-import random
+"""
+A simple hangman game that receives guesses via serial connection.
+Usage: python hangman.py [COM port] [word]
+"""
+
+
 import serial
 import UI
 import pygame
@@ -38,39 +43,48 @@ class Hangman():
 
 		self.createWord()
 
-		display.blit(self.stages[self.fails], (0, 0)) 				# display image
-		display_surface.blit(self.text_word, self.rect_word) 		# display word 
-		display_surface.blit(self.text_guessed, self.rect_guessed) 	# display guessed letters word 
+		# Display the image, word and failed guesses
+		display.blit(self.stages[self.fails], (0, 0))
+		display.blit(self.text_word, self.rect_word)
+		display.blit(self.text_guessed, self.rect_guessed)
 
 	def createWord(self):
 		""" Creates a text object with the word and guessed letters"""
 		show = ""
+
+		# For each letter in the word check if it was guessed
 		for letter in self.word:
 			if letter in self.guessed:
 				show += letter + " "
 			else:
 				show += "_ "
 
+		# Pygame text with the word
 		self.text_word = self.font.render(show, True, (0,0,0)) 
 		self.rect_word = self.text_word.get_rect(center=(self.size[0] // 2, 3*self.size[1] // 4))
 
+		# Get the wrong guesses
 		show = ""
 		for letter in self.guessed:
 			if letter not in self.word:
 				show += letter + " "
 
+		# Pygame text with the wrong guesses
 		self.text_guessed = self.font.render(show, True, (0,0,0)) 
 		self.rect_guessed = self.text_guessed.get_rect(center=(self.size[0] // 2, self.size[1] // 10))
 
 	def checkGuess(self, guess):
 		""" Checks if a guess is right or not """
 
-		if guess in self.guessed: #check if the player repeated some letter
+		# Check if the player repeated some letter
+		if guess in self.guessed:
 			print("You've already said that letter")
 			return False
 
 		else:
-			self.guessed.append(guess) #will add the letter to the list of letters already guessed
+			# Add the letter to the list of letters already guessed
+			self.guessed.append(guess)
+
 			if guess not in self.word:
 				self.fails += 1
 				return False
@@ -83,43 +97,56 @@ class Hangman():
 
 		self.stages = []
 
+		# Search for all images in the respective folder and load them
 		for f in os.listdir(path):
 			ext = os.path.splitext(f)[1]
 			if ext.lower() == ".png":
 				self.stages.append(pygame.image.load(os.path.join(path,f)))
 
-if __name__ == "__main__":
+def main():
+	from sys import argv
 
+	# Checks for the correct number of arguments
+	if len(argv) < 3:
+		print("Wrong number of arguments. Usage: python hangman.py [COM port] [word]")
+		return
+
+	# Initialise pygame and some colors
 	pygame.init() 
-
 	white = (255, 255, 255) 
 	black = (0, 0, 0)
 
-	# dimensions
+	# Set dimensions of the display
 	SIZE = (1400, 800)
-
-	# set dimensions and window name
 	display_surface = pygame.display.set_mode((SIZE[0], SIZE[1]))
+
+	# Set window name
 	pygame.display.set_caption("Project Morse") 
 
-	# create a font object
+	# Create a font object
 	font = pygame.font.Font('freesansbold.ttf', 100) 
 
+	# Create a MorseText object to display the guesses
 	text = UI.MorseText(7*SIZE[0] // 8, SIZE[1] // 4, font)
 
+	# Initialise serial connection
 	ser = serial.Serial('COM5', 9600)
 
-	game = Hangman(input("Word: "), font, SIZE)
+	# Initialise the game
+	game = Hangman(argv[2], font, SIZE)
 
-	while(True):
+	# Game loop
+	running = True
+	while(running):
+
+		# Check for pygame events (doesn't expect any)
 		for event in pygame.event.get() : 
 			if event.type == pygame.QUIT : 
 
-				pygame.quit()
-				ser.close()
-				quit() 
+				running = False
 
 		if not game.finished:
+			# Verify for new guesses, if there is one, update the text
 			if ser.inWaiting():
 				letter = ser.read(1).decode()
 
@@ -130,13 +157,18 @@ if __name__ == "__main__":
 				game.checkGuess(letter)
 				game.displayGame(display_surface)
 
+			# If the text changed, clean the screen and print the new one
 			if text.changed:
 				display_surface.fill(white)
 				
 				text.display(display_surface)
 				game.displayGame(display_surface)
 
+			# Update the display
 			pygame.display.update()  
 
 	pygame.quit()
 	ser.close()
+
+if __name__ == "__main__":
+	main()
